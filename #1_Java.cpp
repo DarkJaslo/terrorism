e3c0efdcaceb2e1f297abd521929ebd010dc86a4
Jon <jon.campillo@gmail.com>
2023-10-08 14:35:12 +0200

    add Java.cpp

---
+++ #1_Java.cpp
@@ -0,0 +1,14 @@
+#include "util/System.h"
+#define public public:
+typedef std::string String;
+typedef bool boolean;
+
+/*public*/ class Main{
+    public static void main(String args[])
+    {
+        boolean shortestVariableNameInJava = true;
+        if(shortestVariableNameInJava == true){
+            System::out::println("Hello world!");
+        }
+    }
+};
