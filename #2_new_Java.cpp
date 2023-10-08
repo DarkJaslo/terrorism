4bcc22026f9f6a137346a4f7bc0ff8ea1143ef8b
Jon <jon.campillo@gmail.com>
2023-10-08 17:22:07 +0200

    add #2_new_Java.cpp

---
+++ #2_new_Java.cpp
@@ -0,0 +1,17 @@
+#include "util/System.h"
+#define public public:
+#define new
+#define delete
+typedef std::string String;
+typedef bool boolean;
+
+new class Main{ //new
+    new public static void main(new String args[])
+    {
+        ArrayList<String> names = new ArrayList<String>();
+        int n = new Integer(123);
+
+        new String new myString = new new String(new "Life is way too short to be coding in Java, man") new
+        delete new;
+    }
+};
