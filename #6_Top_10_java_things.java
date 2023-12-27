e5c43cef07eca28d88b95e199429a95f6e8c250b
Jon <jon.campillo@gmail.com>
2023-12-27 20:34:47 +0100

    add Top 10 java things

---
+++ #6_Top_10_java_things.java
@@ -0,0 +1,16 @@
+class Top10JavaThings
+{
+    public static void main()
+    {
+        System.out.println("Number 10: It's not the only language, you don't have to use it!");
+        System.out.println("Number 9:  It doesn't come installed by default in most systems");
+        System.out.println("Number 8:  It was used to make probably the best videogame ever, Minecraft!");
+        System.out.println("Number 7:  They already made Minecraft Pocket Edition in a more suited language so you don't have to");
+        System.out.println("Number 6:  It fills your memory for you");
+        System.out.println("Number 5:  Runs on 3 quadrillion devices since 2002");
+        System.out.println("Number 4:  Allows maximum crashability: write once, crash everywhere");
+        System.out.println("Number 3:  The Minecraft devs screwed up so badly that many non-technical people know why Java sucks");
+        System.out.println("Number 2:  It trains your typing speed");
+        System.out.println("Number 1:  public static void main(String args[])");
+    }
+};
