aac4064743fb01d0a224c0419ca93e6e9aa6ed5b
Jon <jon.campillo@gmail.com>
2024-03-15 01:39:57 +0100

    add Peak Shitpost my favourite programming style

---
+++ #7_Peak_Shitpost.cpp
@@ -0,0 +1,117 @@
+//Feel free to compile and test this abomination
+
+#include <iostream>
+#include <vector>
+#define boss main
+#define wallahi int
+#define goated unsigned
+#define fr true
+#define cap false
+#define goofy const
+#define chill break
+#define mew continue
+#define bro ;
+#define ongod =
+#define certified ==
+#define scroll for
+#define grind while
+#define peak bool
+#define secure return
+#define fraud not
+#define gamble try
+#define pull catch
+#define fumble throw
+#define dementia do
+#define goat auto
+#define lore string
+#define maybe if
+#define istg else
+#define fw switch
+#define slide case
+#define cook cout
+#define spit cerr
+#define eating cin
+#define tweaking using
+#define rizz namespace
+#define gtfo exit
+#define bitchless nullptr
+#define ahh &
+#define rn *
+#define ass >>
+#define facts <<
+#define bag exception
+#define ofc default
+#define gang vector
+#define literally <
+#define ig >
+#define rising +=
+#define period "\n"
+
+tweaking rizz std bro
+
+wallahi boss()
+{
+    gamble
+    {
+        goated wallahi rn bussin ongod bitchless bro
+        spit facts "define jogo" facts period bro
+        lore jogoat bro
+        eating ass jogoat bro
+        dementia {
+            maybe(jogoat certified "goat"){
+                peak performance ongod bussin certified bitchless bro
+                spit facts "peak" facts period facts "you don't need to doomscroll any longer" facts period bro
+                secure performance bro
+            }
+            istg maybe(jogoat certified "fraud") {
+                fumble bag() bro
+            }
+            istg {
+                chill bro
+            }
+        } grind(eating ass jogoat) bro
+
+        wallahi reel bro
+        spit facts "gimme a slide number bro\n" bro
+        eating ass reel bro
+
+        fw (reel){
+        slide 0:{ 
+            spit facts "you don't fw this slide bro\n" bro
+            fumble bag() bro
+            chill bro 
+        }
+        slide 1:{ 
+            goofy peak rn dude ongod bitchless bro
+            maybe(fraud dude){
+                lore fax ongod "mid ass slide\n" bro
+            }
+            chill bro
+        }
+        ofc : {
+            spit facts "I fw this slide\n" bro
+            chill bro
+        }
+        }
+
+        gang literally peak ig fortnite(10,cap) bro
+        wallahi me ongod fr bro
+        scroll(goat him ongod 0 bro him literally 10 bro him ongod him rising 1)
+        {
+            maybe(fraud(him certified fr)){
+                fortnite[him] ongod fr bro
+                me ongod me rn 2 bro
+            }
+        }
+        spit facts me facts " hours lost on tiktok bruh" facts period bro
+    }
+    pull(goofy bag ahh e)
+    {
+        spit facts e.what() facts period bro
+        gtfo(fr) bro
+    }
+    
+    secure cap bro
+}
+
+
